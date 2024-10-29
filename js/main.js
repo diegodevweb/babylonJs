b3ac2018cc979afd1bfc2689ad7bf2547e866c0b
diegodevweb <diego.devwebb@gmail.com>
2024-10-28 23:30:39 -0300

    feature/start creating a solar system

--- js/main.js
+++ js/main.js
@@ -3,81 +3,59 @@
 // get our canvas
 const canvas = document.getElementById('renderCanvas');
 
-
 // create a BabylonJs engine
 const engine = new BABYLON.Engine(canvas, true);
 
+// camera
+function createCamera(scene) {
+    const camera = new BABYLON.ArcRotateCamera('camera', 0, 0,15, new BABYLON.Vector3.Zero(), scene);
+    //let user move our camera
+    camera.attachControl(canvas);
+
+    // limit camera movement
+    camera.lowerRadiusLimit = 6;
+    camera.upperRadiusLimit = 20;
+}
+
+function createLight(scene) {
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+}
+
+function createSun(scene) {
+    const sun = BABYLON.MeshBuilder.CreateSphere('sun', {segments: 16, diameter: 4}, scene);
+}
+
+function createPlanet(scene) {
+    const planetMaterial = new BABYLON.StandardMaterial('planetMaterial', scene);
+    planetMaterial.diffuseTexture = new BABYLON.Texture('/assets/images/sand.png', scene);
+    // tira o reflexo do planeta
+    planetMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+
+    const planet = BABYLON.MeshBuilder.CreateSphere('planet', {segments: 16, diameter: 1}, scene);
+    planet.material = planetMaterial;
+    planet.position.x = 4.5;
+}
 
 function createScene() {
     // create a scene
     const scene = new BABYLON.Scene(engine);
 
-    // types of cameras
-    // const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, -25), scene);
-    // const camera = new BABYLON.UniversalCamera('camera', new BABYLON.Vector3(0, 0, -25), scene);
-    const camera = new BABYLON.FollowCamera('camera', new BABYLON.Vector3(0, 0, -25), scene);
-    camera.radius = 2;
-    camera.attachControl(canvas, true);
+    createCamera();
 
-    // types of lights
-    // const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
-    // const light = new BABYLON.PointLight('light', new BABYLON.Vector3(0,5,0), scene);
-    const light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(5,-1,4), scene);
+    createLight(scene);
 
-    // create a box
-    const box = new BABYLON.MeshBuilder.CreateBox('box', {
-        size: 2,
-        depth: 2,
-        width: 2.5,
-        height: 1
-    }, scene);
-    box.rotation = new BABYLON.Vector3(2, 0.5, 0.5);
-    camera.lockedTarget = box;
+    createSun(scene);
 
-     // create a sphere
-     const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {}, scene);
-     sphere.position = new BABYLON.Vector3(2.5, 2.5, 0);
-
-    // create a plane
-    const plane = new BABYLON.MeshBuilder.CreatePlane('plane', {}, scene);
-    plane.position.y = -5;    
-    plane.position.x = -3.5;    
-
-    // create a tiled box
-    const tiledBox = BABYLON.MeshBuilder.CreateTiledBox('tiledBox', {}, scene);
-    tiledBox.position = new BABYLON.Vector3(0, 2.5, 0);
-
-    // create a line
-    const points = [
-        new BABYLON.Vector3(2, 0, 0),
-        new BABYLON.Vector3(2, 2, -1),
-        new BABYLON.Vector3(2, 1, 0)
-    ];
-
-    const lines = BABYLON.MeshBuilder.CreateLines('lines', {points}, scene);
-
-    // create a material
-    const material = new BABYLON.StandardMaterial('material', scene);
-    material.diffuseColor = new BABYLON.Color3(0, 1, 1);
-
-    // material.emissiveColor = new BABYLON.Color3(0, 1, 0);
-
-    box.material = material;
-
-    // create a texture
-    const material2 = new BABYLON.StandardMaterial('material2', scene);
-    material2.diffuseTexture = new BABYLON.Texture('assets/images/dark_rock.png', scene);
-
-    sphere.material = material2;
+    createPlanet(scene);
 
     return scene;
 }
 
 // create a instance of scene
-const scene = createScene();
+const mainScene = createScene();
 
 engine.runRenderLoop(() => {
-    scene.render();
+    mainScene.render();
 });
 
 
